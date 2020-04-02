b1457f60bbd61d06e36253c1c28cbe2f49c275ef
msq <zanmoqing@qq.com>
2020-04-02 11:53:03 +0800

    admin 加入监控

--- actuator/src/main/java/com/skin/actuator/ActuatorApplication.java
+++ actuator/src/main/java/com/skin/actuator/ActuatorApplication.java
@@ -1,9 +1,11 @@
 package com.skin.actuator;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class ActuatorApplication {
 
     public static void main(String[] args) {
